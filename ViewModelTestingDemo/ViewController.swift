27a8c9d1ed83409267dd9c60232222637d34c7f5
Bryan Weber <bweber@intrepid.io>
2016-10-25 21:30:12 -0400

    Initial Commit

---
+++ ViewModelTestingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewModelTestingDemo
+//
+//  Created by Bryan Weber on 10/25/16.
+//  Copyright © 2016 Bryan Weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
