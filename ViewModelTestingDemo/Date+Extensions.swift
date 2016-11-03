e07232fb99d98d60527232c621f28e1e16eaae07
Bryan Weber <bweber@intrepid.io>
2016-11-03 08:58:07 -0400

    Add tests for Date extension

--- ViewModelTestingDemo/Date+Extensions.swift
+++ ViewModelTestingDemo/Date+Extensions.swift
@@ -12,10 +12,8 @@ extension Date {
     
     init?(dateString: String) {
         let dateStringFormatter = DateFormatter()
-        dateStringFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-        
-        guard let locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale? else { return nil }
-        dateStringFormatter.locale = locale
+        dateStringFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"        
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale!
         
         guard let date = dateStringFormatter.date(from: dateString) else { return nil }
         self.init(timeInterval:0, since:date)
